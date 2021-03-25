527a5130007fdf1e2786cd484a67223dae04d3a0
Kajetan Kaczmarek <kajkacz@wp.pl>
2021-03-25 18:06:17 +0100

    Reorganized folders

--- MongoDB/get_results.bat
+++ MongoDB/get_results.bat
@@ -1,3 +1,4 @@
 FOR /L %%A IN (1,1,10) DO (
-    mongo nbd zapytanie_%%A.js >> wyniki_%%A.json
+    mongo nbd zapytanie_%%A_MR.js >> wyniki_%%A_MR.json
+    mongo nbd zapytanie_%%A_A.js >> wyniki_%%A_A.json
 )
